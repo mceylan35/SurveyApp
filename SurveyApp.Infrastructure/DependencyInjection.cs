265c0c39d9f22207da53135130fe67520ce0c9bb
mceylan35 <mehmetceylan2696@gmail.com>
2024-11-22 12:59:36 +0300

    add infrastructure

---
+++ SurveyApp.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SurveyApp.Application.Common.Interfaces.Repositories;
+using SurveyApp.Application.Common.Interfaces;
+using SurveyApp.Application.Common.Interfaces.Services;
+using SurveyApp.Infrastructure.Authentication;
+using SurveyApp.Infrastructure.Persistence.MongoDb;
+using SurveyApp.Infrastructure.Persistence.Repositories;
+using SurveyApp.Infrastructure.Security;
+using SurveyApp.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+   
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace SurveyApp.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+
+            services.Configure<MongoDbSettings>(configuration.GetSection(nameof(MongoDbSettings)));
+            services.Configure<MongoDbSettings>(
+             configuration.GetSection("MongoDbSettings"));
+
+            services.AddSingleton<IMongoDbContext, MongoDbContext>();
+             
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<ISurveyRepository, SurveyRepository>();
+            services.AddScoped<IOptionRepository, OptionRepository>();
+            services.AddScoped<IVoteRepository, VoteRepository>();
+
+         
+            services.AddSingleton<IPasswordHasher, PasswordHasher>();
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+
+           
+            var jwtSettings = new JwtSettings();
+            configuration.GetSection(nameof(JwtSettings)).Bind(jwtSettings);
+            services.Configure<JwtSettings>(configuration.GetSection(nameof(JwtSettings)));
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(jwtSettings.Secret))
+                };
+            });
+
+          
+            services.AddHttpContextAccessor();
+            services.AddScoped<ICurrentUserService, CurrentUserService>();
+
+            return services;
+        }
+    }
+}
---
+++ SurveyApp.Infrastructure/Extensions/MongoCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using SurveyApp.Application.Common;
+using SurveyApp.Domain.Attributes;
+using SurveyApp.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyApp.Infrastructure.Extensions
+{
+    public static class MongoCollectionExtensions
+    {
+        public static string GetCollectionName<T>() where T : MongoEntity
+        {
+            var attribute = typeof(T).GetCustomAttribute<BsonCollectionAttribute>();
+
+            if (attribute == null)
+                throw new ArgumentException($"{typeof(T).Name} does not have BsonCollectionAttribute");
+
+            return attribute.CollectionName;
+        }
+    }
+}
