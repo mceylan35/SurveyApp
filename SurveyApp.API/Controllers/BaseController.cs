bbf1e5fa733209da74ecc3186010f7a65f1402da
mceylan35 <mehmetceylan2696@gmail.com>
2024-11-22 12:55:51 +0300

    add survey controller

---
+++ SurveyApp.API/Controllers/BaseController.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SurveyApp.Application.Common.Results;
+
+namespace SurveyApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public abstract class BaseController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BaseController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        protected IActionResult HandleResult<T>(Result<T> result)
+        {
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
+        }
+
+        protected IActionResult HandleCreatedResult<T>(Result<T> result)
+        {
+            return result.IsSuccess
+                ? Ok(result)
+                : BadRequest(result);
+        }
+
+        protected IActionResult HandlePagedResult<T>(Result<T> result)
+        {
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
+        }
+    }
+}
---
+++ SurveyApp.API/Controllers/SurveysController.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SurveyApp.Application.Features.Surveys.Commands.CreateSurvey;
+using SurveyApp.Application.Features.Surveys.Commands.VoteSurvey;
+using SurveyApp.Application.Features.Surveys.Queries.GetSurveyDetail;
+using SurveyApp.Application.Features.Surveys.Queries.GetSurveyList;
+
+namespace SurveyApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SurveysController : BaseController
+    {
+        IMediator _mediator;
+        public SurveysController(IMediator mediator) : base(mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] GetSurveyListQuery query)
+        {
+            var result = await _mediator.Send(query);
+            return HandleResult(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateSurveyCommand command)
+        {
+            return HandleCreatedResult(
+                await _mediator.Send(command));
+        }
+
+        [HttpPost("{id}/vote")]
+        public async Task<IActionResult> Vote(Guid id, VoteSurveyCommand command)
+        {
+            if (id != command.SurveyId)
+                return BadRequest("Invalid survey ID");
+
+            return HandleResult(await _mediator.Send(command));
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDetail(Guid id)
+        {
+            var query = new GetSurveyDetailQuery(id); 
+            return HandleResult(await _mediator.Send(query));
+        }
+    }
+}
+
